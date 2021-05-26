9fa054b1fb364038b81efae5419859cb1ee19a0b
JongChanLee <76044533+JongChanLee01@users.noreply.github.com>
2021-05-26 13:59:24 +0900

    react study

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
     id: 1,
     name: "Kimchi",
-    image: "https://img.hankyung.com/photo/202012/99.23222843.1.jpg"
+    image: "https://img.hankyung.com/photo/202012/99.23222843.1.jpg",
+    rating: 5
   },
   {
     id: 2,
     name: "ramen",
-    image: "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg"
+    image: "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg",
+    rating: 4.9
   },
   {
     id: 3,
     name: "samgiopsal",
-    image: "https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg"
+    image: "https://cdn.mindgil.com/news/photo/202103/70839_7148_1250.jpg",
+    rating: 4.8
   },
   {
     id: 4,
     name: "chukumi",
-    image: "https://blog.kakaocdn.net/dn/dh9Dqr/btqzsWLoYqY/KgEhm09QyxfW29QfeWlkWk/img.png"
+    image: "https://blog.kakaocdn.net/dn/dh9Dqr/btqzsWLoYqY/KgEhm09QyxfW29QfeWlkWk/img.png",
+    rating: 4.7
   },
   {
     id: 5,
     name: "kimbap",
-    image: "https://cdn.fneyefocus.com/news/photo/202105/15991_19264_5337.jpg"
+    image: "https://cdn.fneyefocus.com/news/photo/202105/15991_19264_5337.jpg",
+    rating: 4.6
   },
 ]
 
@@ -42,13 +48,20 @@ const foodILike = [
 // function Food({fav}) {
 //   return <h1>I like {fav}</h1>
 // }
-function Food({name, picture}) {
+function Food({name, picture, rating}) {
   return (
     <div>
       <h1>I like {name}</h1>
-      <img src = {picture} />
+      <h4>{rating}/5.0</h4>
+      <img src = {picture} alt = {name}/>
     </div>
   );
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
 }
 
 /* 
@@ -58,6 +71,9 @@ function renderFood(dish) {
   return <Food key={dish.id} name={dish.name} picture={dish.image}/>
 }
 */
+Food.propTypes = {
+
+}
 
 function App() {
   return (
@@ -68,7 +84,12 @@ function App() {
       {/* 여기서 17번줄 Food 함수에서 보면 key 프롭스는 Food함수로 전달되지 않음 */}
       {/* 이것은 기본적으로 react 내부에서 사용하기 위함임 */}
       {foodILike.map(dish => (
-          <Food key={dish.id} name = {dish.name} picture = {dish.image} />
+          <Food
+            key={dish.id}
+            name = {dish.name}
+            picture = {dish.image}
+            rating = {dish.rating}
+            />
         ))
       }
       {/* ♠ */}

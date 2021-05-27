58e2887c281920d75762d32ee28783624fb1008e
JongChanLee <76044533+JongChanLee01@users.noreply.github.com>
2021-05-27 13:47:06 +0900

    movie_app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 // component를 만들 때마다 모든 것을 다 구현하지 않기 위해서 extend from을 함
 // react component에서 확장을 하였기 때문에 render method를 사용.
@@ -10,10 +11,23 @@ class App extends React.Component{
     isLoading: true,
     movies: []
   };
+
+  // js에게 componentDidMount함수가 끝날 때까지 시간이 약간 걸릴 수 있다고 알려주어야함.
+  // 그걸 위해서
+  // async componentDidMount(){
+  //   const movies = axios.get("https://yts-proxy.now.sh/list_movies.json");
+  // }
+  // 이렇게 해주거나 아래처럼 하면됨
+
+  getMovies = async() => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+  }
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({isLoading: false, book: true});
-    }, 6000);
+    this.getMovies();
+
+    // 밑에 터미널 창에서 ---- npm i axios
+    // js에서 data를 fetch하는 방법은 fetch를 사용함. 하지만 더 좋은 방법으론 Axios가 있음
+    // axios는 fetch위에 있는 작은 layer같은 것
   }
   render(){
     const {isLoading} = this.state;
